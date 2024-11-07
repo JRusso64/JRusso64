f231f4265583626291c87052f2827b6470d544d0
John Russo <jrusso0604@gmail.com>
2024-11-07 18:03:55 -0500

    formatted main page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import * as React from 'react';
 import AppBar from './components/CustomAppBar/CustomAppBar';
 import SideBar from './components/CustomDrawer/ResponsiveDrawer';
+import MainContent from './components/MainContent/MainContent';
 import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid2';
+import Grid from '@mui/material/Grid';
 
 function App() {
   return (
     <>
       <Box sx={{ flexGrow: 1 }}>
+        <AppBar />
+
         <Grid container spacing={2}>
-          <Grid size={2}>
+          <Grid item xs={2}>
             <SideBar />
           </Grid>
-          <Grid size={8}>
-            <AppBar />
+
+          <Grid item xs={8}>
+            <Box 
+              display="flex" 
+              justifyContent="center" 
+              alignItems="center" 
+              height="100vh"
+            >
+              <MainContent />
+            </Box>
           </Grid>
+          <Grid item xs={2} />
         </Grid>
       </Box>
     </>
--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -1,8 +1,12 @@
+import Box from '@mui/material/Box';
+
 const MainContent = () => {
     return  (
-        <>
-        </>
+        <Box>
+            <h1>Top Projects</h1>
 
+            <h1>Smaller Projects</h1>
+        </Box>
     )
 
 };
